c236bf198d2ebaa2f9f100fad531592e0e81dead
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-01 12:35:36 +0300

    Добавьте файлы проекта.

---
+++ Train-Project/Program.cs
@@ -0,0 +1,32 @@
+﻿using Train_Project;
+
+Work work = new();
+void Menu()
+{
+    Console.WriteLine();
+    Console.WriteLine($"1) Добавить пользователя оставевшего коментарий");
+    Console.WriteLine($"2) Вывести список пользователей");
+    Console.WriteLine();
+}
+void GetMenu()
+{
+    while (true)
+    {
+        Menu();
+        string enter = Console.ReadLine();
+        bool check = int.TryParse(enter, out var result);
+        if (result == 1)
+        {
+            work.SetUsers();
+        }
+        else if (result == 2)
+        {
+            work.CheckingAvailabilityUsers();
+        }
+        else
+        {
+            Console.WriteLine($"Выберите пункт из меню");
+        }                
+    }
+}
+GetMenu();
---
+++ Train-Project/Work.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train_Project
+{
+    internal class Work
+    {
+        private List<string> strings=new();
+        public static class Chack
+        {
+            public static void Likes(string[] name)
+            {
+                
+            }
+        }
+        public void SetUsers()
+        {
+            Console.WriteLine($"Введите имя оценившего");
+            string enter = Console.ReadLine();
+            strings.Add(enter);
+        }
+        public void CheckingAvailabilityUsers()
+        {
+            if ( strings.Count==0)
+            {
+                Console.WriteLine($"Никто не оценил");
+            }
+            else if (strings.Count == 1||strings.Count==0)
+            {
+                Console.WriteLine($"{strings[0]} оценил запись");
+            }
+            else
+            {
+                for (int i = 0; i < strings.Count; i++)
+                {
+                    Console.Write($"{strings[i]}");
+                    if (i == strings.Count - 2)
+                    {
+                        Console.Write($" and ");
+                    }
+                    else if (i == strings.Count-1)
+                    {
+                        Console.Write($" оценили запись");
+                    }
+                    else
+                    {
+                        Console.Write($", ");
+                    }
+                }
+            }
+        }
+    }
+}
